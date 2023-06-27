54cf452fb2b59b2d50789b48f86310b6470af967
Decagon <decagon@Decagons-MacBook-Air.local>
2023-06-27 18:05:18 +0100

    modified the layout to add the Header compoenent

--- dashboard/src/Components/Shared/Layout.jsx
+++ dashboard/src/Components/Shared/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import Header from './Header';
 
 export default function Layout() {
     return (
         <div className='flex flex-row bg-neutral-200 h-screen w-screen overflow-hidden'>
             <Sidebar />
-            <div className='p-4 '>
-                {/* <div className='bg-gray-200'>header</div> */}
-                <div className=''>{<Outlet/>}</div>
+            <div className='flex-1'>
+                <Header />
+                <div className='p-4'>{<Outlet/>}</div>
             </div>
         </div>
     )
